ea9f7894e6dc1706bef0c85d4c4f7e1916988132
Meghana Tamrakar <tamrakarmeghana@gmail.com>
2025-10-19 16:35:07 +0530

    Configured GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // important for Netlify relative paths
+  base: '/Companies-Directory/', // 👈 must match your repo name (case-sensitive)
 })
